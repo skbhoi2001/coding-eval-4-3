703bc1002f5a7e0be515a2ae52d9232bfa2e1d6d
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-01-05 22:43:57 +0530

    2

--- evaluation/src/components/pages/Search.jsx
+++ evaluation/src/components/pages/Search.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { logoutSuccess } from "../../redux/login/action"
 
 const Search = ()=>{
+    const [username,setUserName] = useState()
 
     const dispatch = useDispatch()
 
     const handleLogout = () =>{
         dispatch(logoutSuccess())
     }
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if(!username){
+            alert('Please enter name')
+            return
+        }
+        return alert(`${username}`)
 
+        
+    }
     return(
         <div>
             <div>
                 <button onClick={handleLogout}>Logout</button>
             </div>
-            <h2>Search</h2>
+            <div>
+                <input type="text" onChange={e => setUserName(e.target.value)} />
+                <button onClick={handleSubmit}>Search</button>
+            </div>
         </div>
     )
+
+    
 }
 
 export default Search
